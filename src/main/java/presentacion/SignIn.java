6d4b6405652b2a39ac7be932aeb2fd6a33b2c2a6
Estrella Melissa Orellana Cruz <orellanaestrella66@gmail.com>
2025-06-09 11:22:39 -0600

    Avance formulario Sign In

---
+++ src/main/java/presentacion/SignIn.java
@@ -0,0 +1,10 @@
+package presentacion;
+
+import javax.swing.*;
+
+public class SignIn {
+    private JTextField txtNombre;
+    private JTextField txtEmail;
+    private JTextField txtContraseña;
+    private JButton btnCrearCuenta;
+}
