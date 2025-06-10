0c9cedb3b6985c8a18f5f241dc33072e4ea206ce
Emely Gabriela Nájera Serrano <23emelynajera@gmail.com>
2025-06-09 18:41:41 -0600

    Primer diseño de Login

--- src/main/java/presentacion/Login.java
+++ src/main/java/presentacion/Login.java
@@ -1,4 +1,12 @@
 package presentacion;
 
+import javax.swing.*;
+
 public class Login {
+    private JPanel mainPanel;
+    private JComboBox cbTipo;
+    private JTextField txtEmail;
+    private JTextField txtPassword;
+    private JButton btnIngresar;
+    private JButton btnRegistrar;
 }
